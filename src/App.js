81a46f856992f5eb09bd6cf79cb2a12b88f1bbb3
Albert Vila Calvo <vilacalvo.albert@gmail.com>
2021-01-28 20:05:46 +0100

    App header styling

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import './App.css'
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="header">
+          <Link className="header-link" to="/">
+            Would you rather?
+          </Link>
+        </header>
+        <nav></nav>
+        <main></main>
+      </div>
+    </Router>
   )
 }
 
