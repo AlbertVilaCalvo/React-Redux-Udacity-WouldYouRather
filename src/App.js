27e34e3dfebef231e4e78cb720fdc4a02717c362
Albert Vila Calvo <vilacalvo.albert@gmail.com>
2021-01-28 21:15:19 +0100

    App - add <nav> menu and <Route>s

--- src/App.js
+++ src/App.js
@@ -1,17 +1,49 @@
 import './App.css'
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Link,
+  NavLink,
+  Route,
+  Switch,
+} from 'react-router-dom'
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <header className="header">
-          <Link className="header-link" to="/">
-            Would you rather?
-          </Link>
-        </header>
-        <nav></nav>
-        <main></main>
+        <div className="App-container">
+          <header className="header">
+            <Link className="header-link" to="/">
+              Would you rather?
+            </Link>
+          </header>
+          <nav>
+            <ul>
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/add">New Question</NavLink>
+              </li>
+              <li>
+                <NavLink to="/leaderboard">Leader Board</NavLink>
+              </li>
+            </ul>
+          </nav>
+          <main>
+            <Switch>
+              <Route path="/add">
+                <p>Add</p>
+              </Route>
+              <Route path="/leaderboard">
+                <p>Leader Board</p>
+              </Route>
+              <Route path="/">
+                <p>Home</p>
+              </Route>
+            </Switch>
+          </main>
+        </div>
       </div>
     </Router>
   )
